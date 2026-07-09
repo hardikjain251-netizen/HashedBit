488ab3b4fcbd4e586f6dbdd212aba81439942013
hardikjain251-netizen <hardikjain251@gmail.com>
2026-07-09 16:10:26 +0530

    Delete Assignment 2/question1.js

--- Assignment 2/question1.js
+++
@@ -1,9 +0,0 @@
-function printEvenNumbers() {
-  console.log("----- Q1: Even numbers from 1 to 100 -----");
-  for (let i = 1; i <= 100; i++) {
-    // A number is even if it leaves no remainder when divided by 2
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
-}
